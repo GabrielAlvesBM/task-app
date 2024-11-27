918f2b734351c13cb75b1aef2f2ee585e1ccb71d
Gabriel Alves <gabrielalves6002@gmail.com>
2024-11-27 20:18:32 -0300

    chore(components): organize files

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,30 @@
 <template>
-  <div>
-    
+  <div class="px-3 py-10 md:px-10">
+    <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
+      <TodoSpinner />
+
+      <TodoFormAdd />      
+
+      <TodoItems />
+      
+      <TodoEmpty />
+      </div>
   </div>
 </template>
 
 <script>
+import TodoSpinner from './components/TodoSpinner.vue';
+import TodoFormAdd from './components/TodoFormAdd.vue';
+import TodoItems from './components/TodoItems.vue';
+import TodoEmpty from './components/TodoEmpty.vue';
+
 export default {
   name: 'App',
   components: {
+    TodoSpinner,
+    TodoFormAdd,
+    TodoItems,
+    TodoEmpty
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
