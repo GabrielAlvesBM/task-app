1f8c232844467c5463fff19582ec636b4dba9116
Gabriel Alves <gabrielalves6002@gmail.com>
2024-11-27 20:56:51 -0300

    feat(TodoItems): TodoItem functional

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -14,7 +14,7 @@
         <input
             type="text"
             placeholder="Digite a sua tarefa"
-            value="Estudar Vue 3"
+            :value="todo.title"
             class="bg-gray-300 placeholder-gray-500 
             text-gray-700 font-light focus:outline-none block w-full appearance-none 
               leading-normal mr-3"
@@ -49,5 +49,11 @@
 
 <script>
   export default {
+    props: {
+      todo: {
+        type: Object,
+        default: () => ({})
+      }
+    }
   }
 </script>
